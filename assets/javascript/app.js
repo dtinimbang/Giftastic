90e99bd9a12cabfc5e9032e649722351a43b3237
don <don.tinimbang@gmail.com>
2018-01-19 23:52:33 -0600

    gifs popup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,61 @@
 
-// $(document).ready(function() {
 
-// var series = ["Shameless", "Game Of Thrones", "The Office", "Stranger Things"];
-
-// function alertMovieName() {
-//     console.log(this);
-//     console.log("*****");
-//     // YOUR CODE GOES HERE!!!
-//     console.log($(this));
-//     console.log("*****");
-//      alert($(this).text());
-//   }
+var movements = ['burpee', 'push up', 'stretch', 'squat', 'rope climb', 'clean and jerk', 'handstand walk'];
 
 
-//       function renderButtons() {
-//         $("#series-buttons").empty();
-
-//         for (var i = 0; i < series.length; i++) {
-
-//           var seriesButton = $("<button>");  
-//           seriesButton.text(seriesAdd[i]);
-//           seriesButton.addClass("seriesAdd");  
-//           seriesButton.attr("data-name", seriesAdd[i]);
-//           $("#series-buttons").append(seriesButton);
-//           console.log(seriesButton);
-//           console.log("********")
-
-//           renderButtons();
-//         }
-//       }
-
-//       $("#add-series").on("click", function(event) {
-//         event.preventDefault();
-
-//         var show = $("#series-input").val().trim();
-
-//         $(series).push(seriesAdd);
-//         renderButtons();
-
-        
-//       });
-
-
-     
-//         function submitButtonClicked() {
-//             var userInput = $('#series-input').val();
-        
-//             if (userInput) {
-//                 $('#series-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
-//             }
-//         }
-
-//       });
-
-
-    //   $(document).on("click", ".movie", alertMovieName);
-
-    //   // Calling the renderButtons function to display the intial buttons
-    //   renderButtons();
-
-
-    var showTitle = ['The Office', 'Game of Thrones','The Walking Dead','DareDevil'];
+var currentGif;
+var pausedGif;
+var animatedGif;
+var stillGif;
 
 
 //creates buttons
-function createButtons(){
-	$('#TVButtons').empty();
-	for(var i = 0; i < showTitle.length; i++){
-		var showBtn = $('<button>').text(showTitle[i]).addClass('showBtn').attr({'data-name': showTitle[i]});
-		$('#TVButtons').append(showBtn);
-	}
+function createButtons() {
+    $('#fitButton').empty();
+    for (var i = 0; i < movements.length; i++) {
+        var fitnessBtn = $('<button>');
+        fitnessBtn.text(movements[i]);
+        fitnessBtn.addClass('fitnessBtn');
+        fitnessBtn.attr({
+            'data-name': movements[i]
+        });
+        $('#fitButton').append(fitnessBtn);
+    }
+    createButtons();
+
+    //displays gifs on click
+    $('.fitnessBtn').on('click', function () {
+        $('.display').empty();
+
+        var movement = $(this).data('name');
+        console.log(movement);
+        //console.log(this);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movement + "&api_key=1apqXVsrQlquOoNnhYwcCGEeOjbuM2kK";
+        $.ajax({
+                url: queryURL,
+                method: 'GET'
+            })
+            .done(function (response) {
+                var results = response.data;
+                // console.log(response)
+                $.each(results, function (index, value) {
+                    // this produces the picture
+                    animatedGif = value.images.original.url;
+                    //console.log(animatedGif);
+                    pausedGif = value.images.original_still.url;
+                    var thisRating = value.rating;
+                    //gives blank ratings 'unrated' text
+                    if (thisRating == '') {
+                        thisRating = 'unrated';
+                    }
+                    var rating = $('<h5>').html('Rated: ' + thisRating).addClass('ratingStyle');
+                    stillGif = $('<img>').attr('data-animated', animatedGif).attr('data-paused', pausedGif).attr('src', pausedGif).addClass('playOnHover');
+                    var fullGifDisplay = $('<button>').append(rating, stillGif);
+                    $('.display').append(fullGifDisplay);
+                });
+            });
+    });
+}
+
+createButtons();
 
